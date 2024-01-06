2996cf7a7e18da1ddc720f97411f994f1ee480c8
Alvin Betariza <alvin.b.alfred@gmail.com>
2024-01-06 04:36:48 +0300

    console exception log added

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const winston = require('winston')
 const express = require('express')
 const app = express()
 
-winston.exceptions.handle(new winston.transports.File({ filename: 'uncaughtExceptions.log' }))
+winston.exceptions.handle(
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'uncaughtExceptions.log' })
+)
 
-require('./startup/routes')(app)
-require('./startup/db')()
 require('./startup/config')()
+require('./startup/db')()
+require('./startup/routes')(app)
 require('./startup/clientDataValidation')()
 
 const port = process.env.PORT || 7500
